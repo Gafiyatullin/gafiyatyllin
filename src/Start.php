804e1c56cf5094a54559c8e796320321ba57cd9b
Eduard Gafiyatullin <ehduard.gafiyatillin@gmail.com>
2017-07-26 01:00:41 +0300

    Init commit

---
+++ src/Start.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace gafiyatullin\start;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Start
+ *
+ * @author Eduard
+ */
+class Start implements StartInterface{
+    //put your code here
+    public function getName() {
+        echo 'Go';
+    }
+}
---
+++ src/StartInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace gafiyatullin\start;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Eduard
+ */
+interface StartInterface {
+    //put your code here
+    public function getName();
+}
